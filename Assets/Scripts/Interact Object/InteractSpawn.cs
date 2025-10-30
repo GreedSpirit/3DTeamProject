36b61c0faf1643380b4869afda4dd4ca0a22afca
Lux3k <101802736+Lux3k@users.noreply.github.com>
2025-10-30 15:49:26 +0900

    [Feat]상호작용 오브젝트 마구 뿌리기
    상호작용 오브젝트 스포너 자식으로 종속된 트랜스폼들중 랜덤한 위치에 상호작용 오브젝트를 생성하는 기능을 추가했습니다.
    
    인스펙터에서 rand amount 조절시 힐킷,불릿박스 중 무작위로 선택하여 랜덤위치에 생성하고,
    
    HealKit Amount / Bullet Amount 조절시 해당 오브젝트들을 설정값만큼 랜덤 위치에 생성합니다
    
    스포너에 종속된 자식들의 위치를 변경하거나 자식을을 추가 혹은 제거하여 스폰위치를 변경하거나 스폰위치 수를 늘릴 수 있습니다.

--- Assets/Scripts/Interact Object/InteractSpawn.cs
+++ Assets/Scripts/Interact Object/InteractSpawn.cs
@@ -1,28 +1,61 @@
-using System.Collections;
+
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class InteractSpawn : MonoBehaviour
 {
+    public enum InteractType
+    {
+        BulletBox,
+        Healkit,    
+        Rand,
+    }
+    List<Transform> _transforms = new List<Transform>();
+    [SerializeField] private GameObject[] _InteractablePrefabs;
 
-    List<Transform> transforms = new List<Transform>();
-    [SerializeField] private GameObject[] InteractablePrefabs;
+    private List<GameObject> _InteractableObjects = new List<GameObject>();
 
-    private List<GameObject> InteractableObjects = new List<GameObject>();
-    
+    [SerializeField] private int _RandAmount = 3;
+    [SerializeField] private int _healKitAmount = 3;
+    [SerializeField] private int _BulletBoxAmount = 3;
+
     void Start()
     {
-        transforms = GetComponentsInChildren<Transform>().ToList();
+        _transforms = GetComponentsInChildren<Transform>().ToList();
+        CreateRandomInteractObjects(_RandAmount, InteractType.Rand);
+        CreateRandomInteractObjects(_BulletBoxAmount, InteractType.BulletBox);
+        CreateRandomInteractObjects(_healKitAmount, InteractType.Healkit);
     }
 
-    public void CreateObject()
+    public void CreateRandomInteractObjects(int num, InteractType type)
     {
-        int rand = Random.Range(0, InteractablePrefabs.Length);
-        GameObject obj = Instantiate(InteractablePrefabs[rand]);
-        InteractableObjects.Add(obj);
-
-
-
+        for (int i = 0; i < num; i++)
+        {
+            CreateRandomInteractObject(type);
+        }
+    }
+    private void CreateRandomInteractObject(InteractType type)
+    {
+        int Index;
+        if (type == InteractType.Rand)
+        {
+            Index = Random.Range(0, _InteractablePrefabs.Length);
+        }
+        else
+        {
+            Index = (int)type;
+        }
+        int positionRand = Random.Range(0, _transforms.Count);
+        foreach (GameObject inter in _InteractableObjects)
+        {
+            if (!inter.activeSelf)
+            {
+                inter.SetActive(true);
+                inter.transform.position = _transforms[positionRand].position; return;
+            }
+        }
+        GameObject obj = Instantiate(_InteractablePrefabs[Index], _transforms[positionRand]);
+        _InteractableObjects.Add(obj);
     }
 }
